a5b5195ea087885707d1ecd72ce6a10369369a03
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-03-13 15:56:25 -0500

    first commit

---
+++ website/app.js
@@ -0,0 +1,25 @@
+/**
+ * @description this code is for creating the child div elements for the parent div with the id "entryHolder".
+ * @returns three new divs with new id attributes.
+ */
+const main = document.querySelector('body');
+
+const entry = document.createElement('div');
+const entryAtt = document.createAttribute('id');
+entryAtt.value = 'entryHolder';
+entry.setAttributeNode(entryAtt);
+
+const childDivs = ["date", "temp", "content"];
+
+for (let i = 0; i < childDivs.length; i++) {
+    let newDiv = document.createElement('div');
+    let newEntryAtt = document.createAttribute('id');
+    newEntryAtt.value = childDivs[i];
+    newDiv.setAttributeNode(newEntryAtt);
+    newDiv.textContent = childDivs[i];
+    entry.appendChild(newDiv);
+}
+
+main.appendChild(entry);
+
+console.log(entry);
