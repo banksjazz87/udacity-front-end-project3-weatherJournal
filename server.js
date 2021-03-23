1207ea544f4a87544fb7df1f4b0068bd96faa69f
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-03-23 05:52:40 -0400

    docs: still working on the same function as yesterday.

--- server.js
+++ server.js
@@ -27,8 +27,21 @@ function listening() {
     console.log(`running on localhost: ${port}`);
 }
 
-let projectData = {};
+let projectData = {
+    date: "poop",
+    temp: "fart",
+    content: "hello"
+};
 let allData = [];
+//This is the data collected from the original API call
+app.get('/https://api.openweathermap.org/data/2.5/weather?zi…d=649b1be56004028c877a56b4fb684127&units=imperial', currentData);
+
+function currentData() {
+    res.send(projecData)
+    console.log(projectData);
+}
+
+
 
 //This is the route made for the post
 app.post("/newZip", newZip);
